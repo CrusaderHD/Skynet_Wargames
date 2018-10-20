b7a53eb1e02bc1997a807995f7cb72603eb1e01d
Hunter Derrick <Hunderri@uat.edu>
2018-10-20 14:47:35 -0700

    Added Namespaces and basic stuff.

--- Skynet_Wargames/WG_Source.cpp
+++ Skynet_Wargames/WG_Source.cpp
@@ -5,3 +5,18 @@ Professor: Tony Hinton
 Wargames Start: 10/20/18  -- Wargames Finish: 10/20/2018
 Project Name: Skynet HK-Aerial - Wargames
 */
+
+
+
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+
+int main()
+{
+	return(0);
+}
